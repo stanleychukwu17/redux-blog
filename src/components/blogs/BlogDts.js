c0b753bdd6810a9e258cf9a5c0d8cb7e9d6032f8
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-27 21:56:45 +0100

    changes

---
+++ src/components/blogs/BlogDts.js
@@ -0,0 +1,24 @@
+const BlogDts = (props) => {
+    return (
+        <div>
+            <div>Latest blogs</div>
+            <div>
+                <div>
+                    <div>Buhari is no longer travelling to uk</div>
+                    <div>
+                        <div className="it_fl">Author: Chukwu Stanley</div>
+                        <div className="it_rl">2 days ago</div>
+                    </div>
+                    <div>
+                        <div>
+                            <p>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</p>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default BlogDts;
