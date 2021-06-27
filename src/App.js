7df8b6839e5f290d7988b10d73154ce7000dd711
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-27 22:18:09 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from './components/header/Header';
+import SideComp from './components/sidebar/SideComp';
 import BlogDts from './components/blogs/BlogDts';
 
 function App() {
   return (
     <div className="">
       <div><Header /></div>
-      <div>
-        <div className="it_fl"></div>
-        <div className="it_fl"><BlogDts /></div>
+      <div className="HmCvr0">
+        <div className="it_fl HmLft_sd"><SideComp /></div>
+        <div className="it_fl HmRgt_sd"><BlogDts /></div>
       </div>
     </div>
   );
---
+++ src/components/sidebar/SideComp.js
@@ -0,0 +1,13 @@
+const SideComp = () => {
+    return (
+        <div>
+            <div>Lastest activites</div>
+            <div>
+                <div><p>Stanley posted a new blog</p></div>
+                <div><p>Stanley posted a new blog</p></div>
+            </div>
+        </div>
+    );
+}
+ 
+export default SideComp;
