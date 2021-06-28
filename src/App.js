21306117fff910326178859ac630f8358e9a2733
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-28 20:19:04 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import Header from './components/header/Header';
 import SideComp from './components/sidebar/SideComp';
 import BlogDts from './components/blogs/BlogDts';
@@ -5,13 +7,15 @@ import BlogDts from './components/blogs/BlogDts';
 
 function App() {
   return (
-    <div className="">
-      <div><Header /></div>
-      <div className="HmCvr0">
-        <div className="it_fl HmLft_sd"><SideComp /></div>
-        <div className="it_fl HmRgt_sd"><BlogDts /></div>
+    <Router>
+      <div className="">
+        <div><Header /></div>
+        <div className="HmCvr0">
+          <div className="it_fl HmLft_sd"><SideComp /></div>
+          <div className="it_fl HmRgt_sd"><BlogDts /></div>
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
