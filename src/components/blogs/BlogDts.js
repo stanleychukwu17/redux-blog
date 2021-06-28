dfd914e2d517738384cf2dbd1f12a233c4039254
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-28 10:19:09 +0100

    changes

--- src/components/blogs/BlogDts.js
+++ src/components/blogs/BlogDts.js
@@ -6,23 +6,19 @@ const BlogDts = (props) => {
             <div className="Blghdr">Latest blogs</div>
             <div className="">
                 <div className="BlgEch">
-                    <div>Buhari is no longer travelling to uk</div>
-                    <div><div className="it_fl">Author: Chukwu Stanley</div> <div className="it_rl">2 days ago</div></div>
-                    <div>
-                        <div>
-                            <p>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</p>
-                            <p></p>
-                        </div>
+                    <div className="Blgtop">Buhari is no longer travelling to uk</div>
+                    <div className="Blgtt2"><div className="it_fl">Author: Chukwu Stanley</div> <div className="it_rl">2 days ago</div></div>
+                    <div className="BlgInr">
+                        <p>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</p>
+                        <p></p>
                     </div>
                 </div>
                 <div className="BlgEch">
-                    <div>Buhari is no longer travelling to uk</div>
-                    <div><div className="it_fl">Author: Chukwu Stanley</div> <div className="it_rl">2 days ago</div></div>
-                    <div>
-                        <div>
-                            <p>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</p>
-                            <p></p>
-                        </div>
+                    <div className="Blgtop">Buhari is no longer travelling to uk</div>
+                    <div className="Blgtt2"><div className="it_fl">Author: Chukwu Stanley</div> <div className="it_rl">2 days ago</div></div>
+                    <div className="BlgInr">
+                        <p>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</p>
+                        <p></p>
                     </div>
                 </div>
             </div>
