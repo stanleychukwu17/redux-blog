78dfb752f250b5f531cc20052b6d67c28f6026b8
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-28 09:57:22 +0100

    changes

--- src/components/blogs/BlogDts.js
+++ src/components/blogs/BlogDts.js
@@ -5,12 +5,19 @@ const BlogDts = (props) => {
         <div className="dahlah">
             <div className="Blghdr">Latest blogs</div>
             <div className="">
-                <div>
+                <div className="BlgEch">
                     <div>Buhari is no longer travelling to uk</div>
+                    <div><div className="it_fl">Author: Chukwu Stanley</div> <div className="it_rl">2 days ago</div></div>
                     <div>
-                        <div className="it_fl">Author: Chukwu Stanley</div>
-                        <div className="it_rl">2 days ago</div>
+                        <div>
+                            <p>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</p>
+                            <p></p>
+                        </div>
                     </div>
+                </div>
+                <div className="BlgEch">
+                    <div>Buhari is no longer travelling to uk</div>
+                    <div><div className="it_fl">Author: Chukwu Stanley</div> <div className="it_rl">2 days ago</div></div>
                     <div>
                         <div>
                             <p>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</p>
