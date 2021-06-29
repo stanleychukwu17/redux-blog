181a0ea434e1be4b31b1fc780e1cdd44e78952e9
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-29 16:40:26 +0100

    changes

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -17,17 +17,17 @@ const LoginPage = (props) => {
             <div className="it_fl LogBamCvr">
                 <div className="LogDkn">Already a user, then Sign in</div>
                 <div>
-                    <div><p>Username</p> <p><input type="text" /></p></div>
-                    <div><p>Password</p> <p><input type="password" /></p></div>
-                    <div><button className="button_blue">Done, Sign-in</button></div>
+                    <div className="LogDinp"><p>Username</p> <p><input type="text" /></p></div>
+                    <div className="LogDinp"><p>Password</p> <p><input type="password" /></p></div>
+                    <div className="LogDBtn"><button className="button_blue">Done, Sign-in</button></div>
                 </div>
             </div>
             <div className="it_fl LogBamCvr">
                 <div className="LogDkn">New user, then join our community</div>
                 <div>
-                    <div><p>Username</p> <p><input type="text" /></p></div>
-                    <div><p>Password</p> <p><input type="password" /></p></div>
-                    <div><button className="button_blue">Register</button></div>
+                    <div className="LogDinp"><p>Username</p> <p><input type="text" /></p></div>
+                    <div className="LogDinp"><p>Password</p> <p><input type="password" /></p></div>
+                    <div className="LogDBtn"><button className="button_blue">Register</button></div>
                 </div>
             </div>
         </div>
