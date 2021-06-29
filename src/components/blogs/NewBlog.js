cc363617f64794881d226d6a2f7f6b721631acbe
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-29 16:58:45 +0100

    changes

--- src/components/blogs/NewBlog.js
+++ src/components/blogs/NewBlog.js
@@ -6,16 +6,16 @@ const NewBlog = (props) => {
     console.log(props);
     // if the user is not logged in, we re-direct to the logging page
     if (!props.logged_in) {
-        return <Redirect to='/login' />;
+        // return <Redirect to='/login' />;
     }
 
     return (
         <div>
-            <div className="Blghdr">Posting a new blog</div>
+            <div className="Blgortp">Posting a new blog</div>
             <div className="BlgorCv">
                 <div className="Nwbg_inps"><p>Title:</p> <p><input type="text" /></p></div>
-                <div className="Nwbg_inps"><p>Author:</p> <p><input type="text" /></p></div>
-                <div className="Nwbg_inps"><p>Content:</p> <p><input type="text" /></p></div>
+                <div className="Nwbg_inps"><p>Author:</p> <p><select><option value="">Stanley</option></select></p></div>
+                <div className="Nwbg_inps"><p>Content:</p> <p><textarea></textarea></p></div>
             </div>
             <div className="BlgBtn"><button className="button_blue">Save new blog</button></div>
         </div>
