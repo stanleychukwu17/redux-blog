3e2e65c9b8d78d687de4d232f9d58046df8a8734
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-29 09:19:57 +0100

    changes

--- src/components/blogs/NewBlog.js
+++ src/components/blogs/NewBlog.js
@@ -1,11 +1,13 @@
+import './NewBlog.css'
+
 const NewBlog = (props) => {
     return (
         <div>
-            <div>Posting a new blog</div>
+            <div className="tTopOvr">Posting a new blog</div>
             <div>
-                <div class=""><p>Title:</p> <p><input type="text" /></p></div>
-                <div><p>Author:</p> <p><input type="text" /></p></div>
-                <div><p>Content:</p> <p><input type="text" /></p></div>
+                <div className="Nwbg_inps"><p>Title:</p> <p><input type="text" /></p></div>
+                <div className="Nwbg_inps"><p>Author:</p> <p><input type="text" /></p></div>
+                <div className="Nwbg_inps"><p>Content:</p> <p><input type="text" /></p></div>
             </div>
             <div><button>Save new blog</button></div>
         </div>
