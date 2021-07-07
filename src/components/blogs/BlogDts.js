b45e6cc4054b8db15c46de5092bdd8ef29c663fc
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-07 01:01:06 +0100

    changes

--- src/components/blogs/BlogDts.js
+++ src/components/blogs/BlogDts.js
@@ -8,43 +8,34 @@ async function fecthBlogs () {
     return fusers;
 }
 
+function BlogEch () {
+    return (
+        <div className="BlgEch">
+            <div className="Blgtop">Buhari is no longer travelling to uk</div>
+            <div className="Blgtt2"><div className="it_fl">Author: Chukwu Stanley</div> <div className="it_rl">2 days ago</div></div>
+            <div className="BlgInr">
+                <div>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</div>
+                <div>
+                    <div className="it_fl BlgcOvr">
+                        <div className="it_fl"><img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/like-512.png" alt="" /></div>
+                        <div className="it_fl BlgcIcon"><img src="https://cdn2.iconfinder.com/data/icons/bitsies/128/Message-256.png" alt="" /></div>
+                    </div>
+                    <div className="it_rl BlgcStats"><div>30 likes</div> <div>20 comments</div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 const BlogDts = (props) => {
-    let jon = useQuery('all_blogs', fecthBlogs);
-    let {data: maria, status} = {'data':'james', 'status':'loading'};
-    console.log(maria, status);
+    let {data, status} = useQuery('all_blogs', fecthBlogs);
 
     return (
         <div className="dahlah">
             <div className="Blghdr">Latest blogs</div>
             <div className="">
-                <div className="BlgEch">
-                    <div className="Blgtop">Buhari is no longer travelling to uk</div>
-                    <div className="Blgtt2"><div className="it_fl">Author: Chukwu Stanley</div> <div className="it_rl">2 days ago</div></div>
-                    <div className="BlgInr">
-                        <div>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</div>
-                        <div>
-                            <div className="it_fl BlgcOvr">
-                                <div className="it_fl"><img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/like-512.png" alt="" /></div>
-                                <div className="it_fl BlgcIcon"><img src="https://cdn2.iconfinder.com/data/icons/bitsies/128/Message-256.png" alt="" /></div>
-                            </div>
-                            <div className="it_rl BlgcStats"><div>30 likes</div> <div>20 comments</div></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="BlgEch">
-                    <div className="Blgtop">Buhari is no longer travelling to uk</div>
-                    <div className="Blgtt2"><div className="it_fl">Author: Chukwu Stanley</div> <div className="it_rl">2 days ago</div></div>
-                    <div className="BlgInr">
-                        <div>Our very sick president has decide to leave Nigeria to seek medical attension overseas so that he can continue to waste our precious money for nothing sake</div>
-                        <div>
-                            <div className="it_fl BlgcOvr">
-                                <div className="it_fl"><img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/like-512.png" alt="" /></div>
-                                <div className="it_fl BlgcIcon"><img src="https://cdn2.iconfinder.com/data/icons/bitsies/128/Message-256.png" alt="" /></div>
-                            </div>
-                            <div className="it_rl BlgcStats"><div>30 likes</div> <div>20 comments</div></div>
-                        </div>
-                    </div>
-                </div>
+                <div>{status}</div>
+                {data && <BlogEch />}
             </div>
         </div>
     );
