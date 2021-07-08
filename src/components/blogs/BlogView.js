1f1d0f4633de333d41c9744eb7a84574526cce93
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-08 10:23:44 +0100

    changes

--- src/components/blogs/BlogView.js
+++ src/components/blogs/BlogView.js
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import { useQuery } from 'react-query';
 import {useParams} from 'react-router-dom'
 import './BlogView.css'
@@ -9,12 +10,15 @@ async function fecthOnlyThisBlog (id) {
     return fusers;
 }
 
-const BlogView = () => {
+function submitComment (obj) {
+    console.log(obj)
+}
+
+const BlogView = (props) => {
     const {id} = useParams();
+    let [comment, setComment] = useState('');
 
-
-    const {data, isLoading, status} = useQuery(['one_blog', id], () => fecthOnlyThisBlog(id), {staleTime: 300000});
-    console.log(data, isLoading, status)
+    const {data, isLoading} = useQuery(['one_blog', id], () => fecthOnlyThisBlog(id), {staleTime: 300000});
 
     return (
         <>
@@ -23,13 +27,17 @@ const BlogView = () => {
                 <>
                     <div className="Blvw_mjrC">
                         <div className="Blvw_th1"><h1>{data.title}</h1></div>
-                        <div className="Blvw_tdts">Emil Jellinek, a European automobile entrepreneur who worked with DMG, created the trademark in 1902, naming the 1901 Mercedes 35 hp after his daughter Mercedes Jellinek. Jellinek was a businessman and marketing strategist who promoted "horseless" Daimler automobiles among the highest circles of society in his adopted home, which, at that time, was a meeting place for the "Haute Volée" of France and Europe, especially in winter. His customers included the Rothschild family and other well-known personalities. But Jellinek's plans went further: as early as 1901, he was selling Mercedes cars in the New World as well, including US billionaires Rockefeller, Astor, Morgan, and Taylor. At a race in Nice in 1899, Jellinek drove under the pseudonym "Monsieur Mercédès", a way of concealing the competitor's real name as was normal and very regularly done in those days. The race ranks as the hour of birth of the Mercedes-Benz brand. In 1901, the name "Mercedes" was registered by Daimler-Motoren-Gesellschaft (DMG) worldwide as a protected trademark.[12] The first Mercedes-Benz brand name vehicles were produced in 1926, following the merger of Karl Benz's and Gottlieb Daimler's companies into the Daimler-Benz company on 28 June of the same year.</div>
+                        <div className="Blvw_tdts">{data.dts}</div>
                     </div>
                     <div>
                         <div className="it_fl Blvw_cb1"><h2>Comments</h2></div>
                         <div className="it_fl Blvw_cb2">
-                            <div><textarea></textarea></div>
-                            <div><button className="button_blue">Post comment</button></div>
+                            <div><textarea value={comment} onChange={(e) => {
+                                setComment(e.target.value);
+                            }}></textarea></div>
+                            <div><button className="button_blue" onClick={(e) => {
+                                submitComment({comment:comment.current});
+                            }}>Post comment</button></div>
                         </div>
                         <div className="it_fl Blvw_cb3"><h2>33kc</h2></div>
                     </div>
