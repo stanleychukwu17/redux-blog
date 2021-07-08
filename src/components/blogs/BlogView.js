cbdce862edb88d760e8e6e0aa17bef9e4ce9e1ef
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-08 06:14:03 +0100

    changes

---
+++ src/components/blogs/BlogView.js
@@ -0,0 +1,11 @@
+const BlogView = () => {
+    return (
+        <div>
+            <div><h1>The blog title man</h1></div>
+            <div>The blog content</div>
+            <div>The comment section</div>
+        </div>
+    );
+}
+ 
+export default BlogView;
