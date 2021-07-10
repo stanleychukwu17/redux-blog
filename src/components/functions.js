6f99a6e2ac86f3365cfdc878294c3d80f4d776c7
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-10 13:22:56 +0100

    changes

--- src/components/functions.js
+++ src/components/functions.js
@@ -1,11 +1,4 @@
-import React from 'react'
-
 export const update_likes = (obj) => {
     console.log(obj);
     console.log('we uptown boi!')
 }
-
-export const Hello = React.memo(({increment}) => {
-    console.log('rendered');
-    return <button onClick={increment}>Hello</button>
-})
