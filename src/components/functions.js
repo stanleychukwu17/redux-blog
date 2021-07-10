72c3afec5870070b72007967a9f7cd65b9330d52
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-10 13:33:16 +0100

    changes

--- src/components/functions.js
+++ src/components/functions.js
@@ -2,9 +2,9 @@
 export const update_likes = async (obj) => {
     let {blogId, ev} = obj;
 
-    ev.target.style.display = 'none';
+    ev.target.style.visibility = 'hidden';
     let dts = await fetch('http://localhost:8000/likes/1').then(re => re.json());
-    ev.target.style.display = 'block';
+    ev.target.style.visibility = 'visible';
 
     console.log(dts);
 }
