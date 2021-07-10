5250efe909dee58867c609305b15eb9139a147e1
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-10 10:09:52 +0100

    changes

--- src/components/sidebar/SideComp.js
+++ src/components/sidebar/SideComp.js
@@ -12,6 +12,10 @@ const SideComp = (props) => {
                     <div><p>Stanley posted a new blog</p></div>
                     <div><p>Stanley posted a new blog</p></div>
                 </div>
+                <div className="ibm_hdr">
+                    <div className="it_fl ibm_Ech"><p>Total blogs</p><p>5</p></div>
+                    <div className="it_fl ibm_Ech"><p>Total likes</p><p>15</p></div>
+                </div>
             </div>
         )
     );
