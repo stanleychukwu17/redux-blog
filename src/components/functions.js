3e516cc767edc52a6cddd1809814f0b1c5ef94d8
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-10 13:45:13 +0100

    changes

--- src/components/functions.js
+++ src/components/functions.js
@@ -3,17 +3,18 @@ export const update_likes = async (obj) => {
     let {blogId, ev} = obj;
 
     ev.target.style.visibility = 'hidden';
-    let dts = await fetch('http://localhost:8000/likes/1').then(re => re.json());
-    ev.target.style.visibility = 'visible';
+    let dts = await fetch('http://localhost:8000/likes/').then(re => re.json());
 
-    let new_likes = dts.total_likes + 1;
+    let new_likes = dts[0].total_likes + 1;
 
-    console.log(dts, new_likes);
+    console.log(dts[0], new_likes);
 
-    fetch('http://localhost:8000/likes/1', {method: 'DELETE'})
+    let del = await fetch(`http://localhost:8000/likes/${dts[0].id}`, {method: 'DELETE'})
 
-    fetch('http://localhost:8000/likes/', {
+    let upd = await fetch('http://localhost:8000/likes/', {
         method: 'POST', headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({'id':1, 'total_likes': new_likes})
-    })
+        body: JSON.stringify({'total_likes': new_likes})
+    });
+
+    ev.target.style.visibility = 'visible';
 }
