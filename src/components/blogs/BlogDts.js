cefe01f6249398d22e244ee82e2efe861f1c7842
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-30 15:01:38 +0100

    changes

--- src/components/blogs/BlogDts.js
+++ src/components/blogs/BlogDts.js
@@ -5,27 +5,41 @@ import { Link } from 'react-router-dom';
 import setup from '../setup'
 import './BlogDts.css';
 
+// fetches the blog from backend
 async function fecthBlogs () {
     const users = await fetch(`${setup.back_end_url}/blogs/all-blogs`);
     const fusers = users.json();
     return fusers;
 }
 
-function BlogEch ({inf}) {
-    let [likes, setLikes] = useState('');
-    let [comments, setComments] = useState('');
+// send the like to the backend
+function likeThisblog (blog_id) {
+    fetch(`${setup.back_end_url}/blogs/like-new-blog`, {
+        mode: 'cors', method:"POST", headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({blog_id})
+    }).then(re => re.json()).then(re => {
+        console.log(re);
+    })
+}
 
-    let link_to = `/BlogPage/${inf.id}`
+function BlogEch ({inf}) {
+    let [likes, setLikes] = useState(inf.likes);
+    let comments = 0;
+
+    let link_to = `/BlogPage/${inf._id}`
 
     return (
-        <div className="BlgEch">
+        <div className="BlgEch" id="">
             <div className="Blgtop"><Link to={link_to}>{inf.title}</Link></div>
             <div className="Blgtt2"><div className="it_fl">Author: {inf.author}</div> <div className="it_rl">{inf.date_p}</div></div>
             <div className="BlgInr">
                 <div>{inf.content}</div>
                 <div>
                     <div className="it_fl BlgcOvr">
-                        <div className="it_fl"><img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/like-512.png" alt="" /></div>
+                        <div className="it_fl"><img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/like-512.png" alt="like" onClick={() => {
+                            setLikes(c => c + 1);
+                            likeThisblog(inf._id)
+                        }} /></div>
                         <div className="it_fl BlgcIcon"><img src="https://cdn2.iconfinder.com/data/icons/bitsies/128/Message-256.png" alt="" /></div>
                     </div>
                     <div className="it_rl BlgcStats"><div>{likes} likes</div> <div>{comments} comments</div></div>
@@ -37,7 +51,6 @@ function BlogEch ({inf}) {
 
 const BlogDts = (props) => {
     let {data, status} = useQuery('all_blogs', fecthBlogs, {staleTime:300000});
-    // console.log(data, status);
 
     return (
         <div className="dahlah">
