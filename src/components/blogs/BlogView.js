e3cccb968c90b7fc4261d034c6df3c60e40fe157
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-08-05 08:41:05 +0100

    changes

--- src/components/blogs/BlogView.js
+++ src/components/blogs/BlogView.js
@@ -58,7 +58,7 @@ const BlogView = (props) => {
                         </div>
                         <div className="it_fl Blvw_cb3"><h2>33kc</h2></div>
                     </div>
-                    <div>All comments</div>
+                    <div className="CmkAtCvr">All comments</div>
                     <div>
                         <div className="CmkOriCvr">
                             <div className="Cmk1Cvr">
