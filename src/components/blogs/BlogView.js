362f54784a51f55b83e035365454b2432b9a93c7
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-08-10 21:40:50 +0100

    changes

--- src/components/blogs/BlogView.js
+++ src/components/blogs/BlogView.js
@@ -51,7 +51,7 @@ const BlogView = (props) => {
     // fetches the indivial blog details
     const {data, isLoading} = useQuery(['one_blog', id], () => fecthOnlyThisBlog(id), {staleTime: 300000}); // 5 mintues of staletime
 
-    // when we receives
+    // when we receive the data from
     useEffect(() => {
         if (data) {
             setTotComments(data.dts.cdts.total)
